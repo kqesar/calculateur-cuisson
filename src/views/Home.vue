7cfb42d874e75ed0e9545ff3a14b6493c936f3be
yfievez <yoann.fievez@gmail.com>
2021-04-29 18:44:29 +0200

    Update calculator

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,76 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col>
-                <h2 class="text-center">Calculateur cuisson</h2>
-            </v-col>
-        </v-row>
-        <v-row class="mb-5">
-            <v-col>
-                <v-form>
+        <v-form>
+            <v-row>
+                <v-col>
+                    <h2 class="text-center">Calculateur cuisson</h2>
+                </v-col>
+            </v-row>
+            <v-row class="mb-3">
+                <v-col>
+                    <h4>Paramétrage type de viande</h4>
+                    <v-row v-for="(type, index) in listTypeCuisson" :key="index">
+                        <v-col>
+                            <v-text-field readonly v-model="listTypeCuisson[index]" label="Type de cuisson"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field v-model="cuissons[type].weight" label="Poids"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field v-model="cuissons[type].duration" label="Durée"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-btn @click="updateCuisson()">Modifier cuisson</v-btn>
+                        </v-col>
+                    </v-row>
+                    <v-row class="mt-3">
+                        <v-col>
+                            <h4>Ajout nouvelle cuisson</h4>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <v-text-field v-model="newTypeCuissonName" label="Nom nouveau type de cuisson"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field v-model="newTypeCuisson.weight" label="Poids nouvelle cuisson"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field v-model="newTypeCuisson.duration" label="Durée nouvelle cuisson"></v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-btn @click="saveCuisson()">Enregistrer nouvelle cuisson</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
+            <v-row class="mb-3">
+                <v-col>
+                    <h4>Choix type de mesure</h4>
                     <v-select v-model="typeMesureDuration" label="Type durée" :items="listTypeMesureDuration"></v-select>
                     <v-select v-model="typeMesureWeight" label="Type poids" :items="listTypeMesureWeight"></v-select>
-                </v-form>
-            </v-col>
-        </v-row>
-        <v-row class="mt-5">
-            <v-col>
-                <v-form>
+                </v-col>
+            </v-row>
+            <v-row class="mt-3">
+                <v-col>
+                    <h4>Calcul de la cuisson</h4>
                     <v-select v-model="typeCuisson" label="Sélectionner type de viande" :items="listTypeCuisson"></v-select>
                     <v-text-field v-if="typeCuisson" v-model="poidsCuisson" label="Poids de la viande" @change="changePoidsCuisson"></v-text-field>
-                    <v-text-field v-if="typeCuisson" v-model="dureeCuisson" label="Durée de cuisson"></v-text-field>
-                </v-form>
-            </v-col>
-        </v-row>
+                    <v-text-field readonly v-if="typeCuisson" v-model="dureeCuisson" label="Durée de cuisson"></v-text-field>
+                </v-col>
+            </v-row>
+        </v-form>
     </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator'
-import { Cuissons } from '@/interfaces'
+import { Cuisson, Cuissons } from '@/interfaces'
 
 @Component({})
 export default class Home extends Vue {
+    newTypeCuisson: Cuisson = {}
+    newTypeCuissonName = ''
     typeCuisson = ''
     poidsCuisson = 0
     dureeCuisson: any = 0
@@ -75,6 +115,18 @@ export default class Home extends Vue {
         this.dureeCuisson = (this.poidsCuisson * this.cuissons[this.typeCuisson].duration) / this.cuissons[this.typeCuisson].weight
     }
 
+    updateCuisson (): void {
+        localStorage.setItem('cuissons', JSON.stringify(this.cuissons))
+    }
+
+    saveCuisson (): void {
+        this.listTypeCuisson.push(this.newTypeCuissonName)
+        this.cuissons[this.newTypeCuissonName] = this.newTypeCuisson
+        localStorage.setItem('cuissons', JSON.stringify(this.cuissons))
+        this.newTypeCuissonName = ''
+        this.newTypeCuisson = {}
+    }
+
     @Watch('typeMesureWeight')
     onTypeMesureWeight (value: string): void {
         if (this.poidsCuisson) {
@@ -98,7 +150,7 @@ export default class Home extends Vue {
         }
     }
 
-    created (): void {
+    mounted (): void {
         this.initializeForm()
     }
 }
