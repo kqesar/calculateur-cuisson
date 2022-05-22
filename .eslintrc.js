1c711d0991e007762cab0a585d901d658b538c94
Yoann Fievez <yoann.fievez@gmail.com>
2022-05-23 00:01:16 +0200

    feat:Optimize build

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
         '@vue/typescript/recommended'
     ],
     parserOptions: {
-        ecmaVersion: 2020
+        ecmaVersion: "latest"
     },
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,21 @@
             dark
         >
             <div class="d-flex align-center">
-                <font-awesome-icon icon="cookie"></font-awesome-icon>
                 <span class="ml-3">Calculateur cuisine</span>
             </div>
         </v-app-bar>
         <v-main>
-            <home/>
+            <calculator/>
         </v-main>
     </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import Home from "@/views/Home.vue";
+import Calculator from "@/views/Calculator.vue";
 
 @Component({
-    components: {Home}
+    components: {Calculator}
 })
 export default class App extends Vue {
 }
