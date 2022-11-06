0688ff0bb1239a06220ae25b90398d7d1cdacebd
Yoann Fievez <yoann.fievez@gmail.com>
2022-11-06 17:40:37 +0100

    feat(): Mise à jour package

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import vuetify from './plugins/vuetify'
 Vue.config.productionTip = false
 Vue.config.devtools = true
 Vue.config.performance = true
-
 new Vue({
     vuetify,
     render: h => h(App)
