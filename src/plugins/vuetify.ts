1793daa4614c08e890cf218fd25840018eedca24
Yoann Fievez <yoann.fievez@gmail.com>
2022-11-06 22:47:47 +0100

    feat: Migrer de vue cli vers vite (#48)

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,13 +1,7 @@
-
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-    components,
-    directives,
-})
+const vuetify = createVuetify()
 
 export default vuetify
