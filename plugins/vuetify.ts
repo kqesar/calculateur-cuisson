5eaa9f3dc081ad272b09634c43a969ae7122ee89
kqesar <yoann.fievez@gmail.com>
2023-06-23 17:46:39 +0200

    feat: Optimize vuetify

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,15 +2,11 @@
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import { defineNuxtPlugin} from "nuxt/app";
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 
 export default defineNuxtPlugin(nuxtApp => {
     const vuetify = createVuetify({
         ssr: true,
-        components,
-        directives,
     })
     nuxtApp.vueApp.use(vuetify)
 })
