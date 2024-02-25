e8a81e75350ea878177222877f5d6ec0a8fb691d
KqesaR <yoann.fievez@gmail.com>
2024-02-25 18:41:10 +0100

    delete old views folder

--- src/views/__tests__/CalculatorView.spec.ts
+++
@@ -1,7 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import CalculatorView from '../CalculatorView.vue'
-import { describe, it, expect } from 'vitest'
-
-describe('CalculatorView', () => {
-  
-})
